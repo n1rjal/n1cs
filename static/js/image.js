051fa182d6d057226c14ec723273285e940d352b
n1rjal <nirjalpaudel54312@gmail.com>
2024-06-23 23:44:58 +0545

    fix: description for productivity

--- static/js/image.js
+++ static/js/image.js
@@ -2,9 +2,16 @@
 const imgs = document.getElementsByTagName("img");
 const fs_img = document.getElementById("fs_img");
 const fs_img_container= document.getElementById("fs_img_container");
+const close_fs = document.getElementById("close_fs");
 
 fs_img.addEventListener("click", (e)=>{
     e.stopPropagation();
+});
+
+
+close_fs.addEventListener("click", ()=>{
+    fs_img.style.display="none";
+    fs_img_container.style.display="none";
 });
 
 fs_img_container.addEventListener("click", ()=>{
