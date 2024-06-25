8e48b131f3bb39fcbbf220211f4bf82f06fbb7a1
n1rjal <nirjalpaudel54312@gmail.com>
2024-06-25 13:39:41 +0545

    feat: add image compress scripts

---
+++ scripts/compress.py
@@ -0,0 +1,46 @@
+import os
+from urllib.parse import quote
+from PIL import Image
+
+def is_image(name):
+    exts = ["png", "jpeg", "jpg"]
+    for ext in exts:
+        if ext in name.lower():
+            return True
+
+    return False
+
+def compress_image(image_path, output_path, quality=85):
+    try:
+        with Image.open(image_path) as img:
+            w, h = img.size
+            new_size = (int(w/1.5), int(h/1.5))
+            img.resize(new_size)
+            img.save(output_path, "webp",optimize=True,  quality=quality,)
+    except Exception as e:
+        print(f"Error compressing {image_path}: {e}")
+
+
+for root, dirs, files in  os.walk("./content/posts", topdown=True):
+    if "index.md" in files and "img" in dirs:
+        content = open(root + "/index.md").read()
+        with os.scandir(root + "/img") as img_dir_entries:
+            for entry in img_dir_entries:
+                if is_image(entry.name):
+                    image_path = root + "/img/" + entry.name
+                    output_path = root + "/img/" + entry.name.split(".")[0] + ".webp"
+                    compress_image(
+                            image_path,
+                            output_path
+                    )
+                    ext = entry.name.split(".")[-1]
+                    image_name = quote(entry.name)
+                    new_image_name= image_name.replace(ext, "webp")
+                    content = content.replace(image_name, new_image_name)
+                    os.remove(image_path)
+
+
+        print("webp" in content.lower())
+        with open(root + "/index.md", "w") as f:
+            f.write(content)
+                    
